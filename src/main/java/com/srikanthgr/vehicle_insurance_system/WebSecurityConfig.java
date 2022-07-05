cafd4adb883c596367b5fa734a04e555647bf31e
Srikanth Govindaraghavan <sri.gr81@gmail.com>
2022-07-06 01:45:40 +0530

    Update WebSecurityConfig.java

--- src/main/java/com/srikanthgr/vehicle_insurance_system/WebSecurityConfig.java
+++ src/main/java/com/srikanthgr/vehicle_insurance_system/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.srikanthgr.vehicle_insurance_system;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
      
     @Override
