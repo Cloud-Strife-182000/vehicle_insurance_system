abafc70ec8dee739c6cfffc4953c98496cc5dbb7
Srikanth Govindaraghavan <sri.gr81@gmail.com>
2022-07-06 00:43:45 +0530

    commit

--- src/main/java/com/srikanthgr/vehicle_insurance_system/WebSecurityConfig.java
+++ src/main/java/com/srikanthgr/vehicle_insurance_system/WebSecurityConfig.java
@@ -1,21 +1,24 @@
 package com.srikanthgr.vehicle_insurance_system;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
- 
-    @Autowired
-    private DataSource dataSource;
      
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**");
+    }
+
+
+    /*
     @Bean
     public UserDetailsService userDetailsService() {
         return new CustomUserDetailsService();
@@ -54,5 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .permitAll()
             .and()
             .logout().logoutSuccessUrl("/").permitAll();
+            
     }
+    */
 }
