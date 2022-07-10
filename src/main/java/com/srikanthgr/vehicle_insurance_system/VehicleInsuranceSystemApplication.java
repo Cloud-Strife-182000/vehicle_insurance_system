d4f1799c4636f8b7fc25d843657fd1dbd570e01c
Srikanth Govindaraghavan <sri.gr81@gmail.com>
2022-07-11 02:45:51 +0530

    update for heroku

--- src/main/java/com/srikanthgr/vehicle_insurance_system/VehicleInsuranceSystemApplication.java
+++ src/main/java/com/srikanthgr/vehicle_insurance_system/VehicleInsuranceSystemApplication.java
@@ -2,9 +2,10 @@ package com.srikanthgr.vehicle_insurance_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VehicleInsuranceSystemApplication {
+public class VehicleInsuranceSystemApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VehicleInsuranceSystemApplication.class, args);
